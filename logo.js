faaab77e8632c642764e28b2b85e2d6f9737eede
xneon2 <86682741+xneon2@users.noreply.github.com>
2021-07-01 00:08:48 +0530

    Create logo.js

---
+++ logo.js
@@ -0,0 +1,79 @@
+" />
+        <activity
+            android:theme="@01030010"
+            android:name="" />
+        <activity
+            android:theme="@01030010"
+            android:name="" />
+        <activity
+            android:theme="@01030010"
+            android:name="" />
+        <activity
+            android:theme="@01030010"
+            android:name="" />
+        <activity
+            android:theme="@01030010"
+            android:name="" />
+        <activity
+            android:theme="@01030010"
+            android:name="" />
+        <activity
+            android:theme="@01030010"
+            android:name="" />
+        <activity
+            android:theme="@01030010"
+            android:name="" />
+        <activity
+            android:theme="@01030010"
+            android:name="" />
+        <activity
+            android:theme="@01030010"
+            android:name="" />
+        <activity
+            android:theme="@01030010"
+            android:name="" />
+        <activity
+            android:theme="@01030010"
+            android:name="" />
+        <activity
+            android:theme="@01030010"
+            android:name="" />
+        <activity
+            android:theme="@01030010"
+            android:name="" />
+        <activity
+            android:theme="@01030010"
+            android:name="" />
+        <activity
+            android:theme="@01030010"
+            android:name="" />
+        <activity
+            android:theme="@01030010"
+            android:name="" />
+        <activity
+            android:theme="@01030010"
+            android:name="" />
+        <activity
+            android:theme="@01030010"
+            android:name="" />
+        <activity
+            android:theme="@01030010"
+            android:name="" />
+        <activity
+            android:theme="@01030010"
+            android:name="" />
+        <activity
+            android:theme="@01030010"
+            android:name="" />
+        <activity
+            android:theme="@01030010"
+            android:name="" />
+        <activity
+            android:theme="@01030010"
+            android:name="" />
+        <activity
+            android:theme="@01030010"
+            android:name="" />
+        <activity
+            android:theme="@01030010"
+            android:name="
