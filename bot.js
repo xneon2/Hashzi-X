e5d6e821fc44d7901b8a148958303d8a187e42fa
xneon2 <86682741+xneon2@users.noreply.github.com>
2021-08-04 21:47:08 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -78,7 +78,7 @@ async function whatsAsena () {
     
     const conn = new WAConnection();
     const Session = new StringSession();
-    conn.version = [2, 2119, 6]
+    conn.version = [2, 2123, 8]
 
     conn.logger.level = config.DEBUG ? 'debug' : 'warn';
     var nodb;
