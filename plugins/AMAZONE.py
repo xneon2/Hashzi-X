b51881860520d4107636482f41cac91325467554
Teenuh-X <86682741+xneon2@users.noreply.github.com>
2021-09-14 21:08:07 +0530

    Create AMAZONE.py

---
+++ plugins/AMAZONE.py
@@ -0,0 +1,7 @@
+/* Copyright (C) 2021 TENUX-Neotro.
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.
+NEOTROX - TEENUHX
+*/
+
+481c863d-be29-4b7f-854c-ea859ea2dacff281ff67-7b39-470a-b7b0-12dc933d8c11481c863d-be29-4b7f-854c-ea859ea2dacff281ff67-7b39-470a-b7b0-12dc933d8c11481c863d-be29-4b7f-854c-ea859ea2dacff281ff67-7b39-470a-b7b0-12dc933d8c11481c863d-be29-4b7f-854c-ea859ea2dacf
