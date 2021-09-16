128c14132a86192cd7774fe0783ea157618d4cfd
Teenuh-X <86682741+xneon2@users.noreply.github.com>
2021-09-17 02:23:03 +0530

    Create qr.js

---
+++ qr.js
@@ -0,0 +1,64 @@
+/* Copyright (C) 2021 TENUX-Neotro.
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.
+NEOTROX - TEENUHX
+*/
+
+const chalk = require('chalk');
+const { WAConnection, MessageType } = require('@adiwajshing/baileys');
+const fs = require('fs');
+async function whatsAsena() {
+  const conn = new WAConnection();
+  conn.logger.level = 'warn';
+  conn.version = [2, 2126, 14]
+
+  conn.on('connecting', async () => {
+    console.log(`${chalk.green.bold('Amazone Alexa')}${chalk.green.bold('Team')}
+${chalk.white.italic('amazone String code recipient')}
+${chalk.blue.bold('ℹ️  Connecting alexa... Please wait.')}`);
+  });
+
+  conn.on('open', async () => {
+    console.log(
+      chalk.green.bold('Alexa QR Code: '),
+      'NEUTRO;;;' +
+      Buffer.from(JSON.stringify(conn.base64EncodedAuthInfo())).toString(
+        'base64'
+      )
+    );
+    await conn.sendMessage(
+      conn.user.jid,
+      'NEUTRO;;;' +
+      Buffer.from(JSON.stringify(conn.base64EncodedAuthInfo())).toString(
+        'base64'
+      ),
+      MessageType.text
+    );
+    if (conn.user.jid.startsWith('91')) {
+      await conn.sendMessage(
+        conn.user.jid,
+        '*~___________~* *'+ conn.user.name + ' ~___________~*\n\n*▪️ Amazone Scanned✅️*\n*▪️Thanks For Choosing Amazone 💞*',
+        MessageType.text
+      );
+    } else {
+      await conn.sendMessage(
+        conn.user.jid,
+        '*~_____________~* *'+ conn.user.name + ' ~_____________~*\n\n*▪️ Amazone Successfully Scanned✅️*\n*▪️Thanks For Choosing Amazone 💞*',
+        MessageType.text
+      );
+    }
+    console.log(
+      chalk.green.bold(
+        "\n\n Nigalkku sandesham pakarthan \n kaliyunnillegil,whatsapp parishodikkuka \n nigalude numberillekku  code ayachinnu!\n\n\n\n"
+      ),
+      chalk.green.bold(
+        'IF YOU CANNOT COPY THE MESSAGE, PLEASE CHECK WHATSAPP. QR CODE SENT TO YOUR OWN NUMBER!'
+      )
+    );
+    process.exit(0);
+  });
+
+  await conn.connect();
+}
+
+whatsAsena();
